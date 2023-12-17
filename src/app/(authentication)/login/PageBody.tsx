8cf5ff1ed8b89b3c6a886ffbef5ea9c548594ec3
Md Kamruzzaman <kamruzzaman-dev@users.noreply.github.com>
2023-12-17 21:48:44 +0600

    login page design done

--- src/app/(authentication)/login/PageBody.tsx
+++ src/app/(authentication)/login/PageBody.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { Button, Form, Input } from "antd";
 import Link from "next/link";
+import style from "../style/authentication.module.css";
 
 
 const PageBody = () => {
@@ -14,59 +15,74 @@ const PageBody = () => {
     };
 
     return (
-        <Form
-            form={form}
-            className=""
-            name="basic"
-            initialValues={{
-                remember: true,
-            }}
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-            autoComplete="off"
-        >
-            <label className="font-medium">Email Address</label>
-            <Form.Item
-                className=""
-                name="email"
-                rules={[
-                    {
-                        type: "email",
-                        message: "Your email address",
-                    },
-                    {
-                        required: true,
-                        message: "The input is not valid E-mail!",
-                    },
-                ]}
-            >
-                <Input className="" />
-            </Form.Item>
-
-            <div className="pt-2">
-                <label className="font-medium">Password</label>
-                <Form.Item
-                    name="password"
-                    rules={[
-                        {
-                            required: true,
-                            message: "Please input your password!",
-                        },
-                    ]}
+        <div className={style.main_container} >
+            <div className={style.form_container}>
+                <Form
+                    form={form}
+                    className=""
+                    name="basic"
+                    initialValues={{
+                        remember: true,
+                    }}
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                    autoComplete="off"
                 >
-                    <Input.Password className="" />
-                </Form.Item>
-            </div>
-            <div className="">
-                <Link href="/reset-password" className="">
-                    Forget your password
-                </Link>
-            </div>
+                    <div>
+                        <label style={{ fontSize: "15px" }} >Email Address</label>
+                        <Form.Item
+                            name="email"
+                            rules={[
+                                {
+                                    type: "email",
+                                    message: "Your email address",
+                                },
+                                {
+                                    required: true,
+                                    message: "The input is not valid E-mail!",
+                                },
+                            ]}
+                        >
+                            <Input style={{ marginTop: "4px", width: "300px", height: "40px", borderRadius: "2px" }} />
+                        </Form.Item>
+                    </div>
 
-            <Form.Item>
-                <Button type="primary" htmlType="submit">Log in</Button>
-            </Form.Item>
-        </Form>
+                    <div style={{ marginTop: "4px" }} >
+                        <label style={{ fontSize: "15px" }} >Password</label>
+                        <Form.Item
+                            name="password"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: "Please input your password!",
+                                },
+                            ]}
+                        >
+                            <Input.Password style={{ marginTop: "4px", width: "300px", height: "40px", borderRadius: "2px" }} />
+                        </Form.Item>
+                    </div>
+
+                    <div style={{ width: "100%", margin: "14px 0px", textAlign: "right" }}>
+                        <Link href="/reset-password" className="">
+                            Forget your password?
+                        </Link>
+                    </div>
+
+                    <div style={{ width: "100%", textAlign: "center", }}>
+                        <Form.Item>
+                            <Button style={{
+                                width: "100%",
+                                borderRadius: "2px",
+                                textAlign: "center",
+                                margin: "auto",
+                                fontSize: 14,
+                                fontWeight: "500",
+                            }} type="primary" htmlType="submit">Log in</Button>
+                        </Form.Item>
+                    </div>
+                </Form>
+            </div>
+        </div>
     );
 };
 
