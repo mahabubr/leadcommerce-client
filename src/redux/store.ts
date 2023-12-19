60ad835963b1a197649fa694765b8afc70e6f02b
Md Kamruzzaman <kamruzzaman-dev@users.noreply.github.com>
2023-12-19 11:34:35 +0600

    redux issue resolved and auth login api added

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -5,7 +5,8 @@ export const store = configureStore({
   reducer: {
     [api.reducerPath]: api.reducer,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
