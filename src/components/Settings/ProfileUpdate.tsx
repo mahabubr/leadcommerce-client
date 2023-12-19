3fd7427ba005aa5ee332cd597058602a968f25e8
shakhawatsalam <shakhawatsalam726@gmail.com>
2023-12-19 09:53:20 +0600

    [password change and remove password card added]

--- src/components/Settings/ProfileUpdate.tsx
+++ src/components/Settings/ProfileUpdate.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ProfileUpdateFrom from "./ProfileUpdateFrom";
 import ProfileImageUpload from "./ProfileImageUpload";
-
+import { Divider } from "antd";
+import { Button, Flex } from "antd";
 const ProfileUpdate = () => {
   return (
     <>
@@ -12,18 +13,80 @@ const ProfileUpdate = () => {
         <div
           style={{
             width: "60%",
-            padding: "3rem",
+            padding: "1rem",
+            // border: "1px solid red",
           }}>
           <ProfileUpdateFrom />
         </div>
         <div
           style={{
             display: "flex",
+            // border: "1px solid red",
             justifyContent: "center",
-            marginTop:"10%",
-            flex: "1",
+            alignItems: "center",
+            width: "40%",
           }}>
           <ProfileImageUpload />
+        </div>
+      </div>
+      <Divider />
+      <div
+        style={{
+          display: "flex",
+
+          justifyContent: "space-between",
+        }}>
+        <div
+          style={{
+            width: "45%",
+          }}>
+          <div
+            style={{
+              height: "100px",
+              padding: "1rem",
+              fontSize: "16px",
+              backgroundColor: "#ecf0f1",
+            }}>
+            <Button
+              size='small'
+              style={{
+                float: "inline-end",
+                backgroundColor: "#2c3e50",
+                color: "#ecf0f1",
+              }}>
+              Change
+            </Button>
+            <h6 style={{ fontSize: "1rem" }}>Password</h6>
+            <small style={{ color: "#2c3e50" }}>
+              You can reset or change your password by clicking here
+            </small>
+          </div>
+        </div>
+        <div
+          style={{
+            width: "45%",
+          }}>
+          <div
+            style={{
+              height: "100px",
+              padding: "1rem",
+              fontSize: "16px",
+              backgroundColor: "#ecf0f1",
+            }}>
+            <Button
+              size='small'
+              style={{
+                float: "inline-end",
+                backgroundColor: "#2c3e50",
+                color: "#ecf0f1",
+              }}>
+              Deactivate
+            </Button>
+            <h6 style={{ fontSize: "1rem" }}>Remove</h6>
+            <small style={{ color: "#2c3e50" }}>
+              Once you delete your account, there is no going back
+            </small>
+          </div>
         </div>
       </div>
     </>
