458cdf0b228efb70853908ab2db5afb4b798900e
shakhawatsalam <shakhawatsalam726@gmail.com>
2024-01-03 22:33:47 +0600

    [card customize done]

--- src/app/(dashboard)/employees/add-employe/page.tsx
+++ src/app/(dashboard)/employees/add-employe/page.tsx
@@ -5,6 +5,17 @@ import AddEmploye from "@/components/Employee/AddEmploye";
 const AddEmployePage = () => {
   return (
     <div style={{ padding: 20, marginTop: 20 }}>
+      <div
+        style={{
+          backgroundColor: "white",
+          padding: "1vw",
+          borderRadius: "8px",
+          marginBottom: "1vw",
+          boxShadow:
+            "rgba(17, 17, 26, 0.05) 0px 1px 0px, rgba(17, 17, 26, 0.1) 0px 0px 8px",
+        }}>
+        <h3>Add Employee</h3>
+      </div>
       <AddEmploye />
     </div>
   );
--- src/components/users/AdminCart.tsx
+++ src/components/users/AdminCart.tsx
@@ -9,30 +9,91 @@ type adminData = {
   email: string;
 };
 function UserCart({ admin }: { admin?: any }) {
-  const { full_name, image, phone, address, email }: adminData = admin
-    ? admin
-    : {};
+  // const { full_name, image, phone, address, email }: adminData = admin
+  //   ? admin
+  //   : {};
   return (
-    <div className={style.Card}>
-      <div className={style.upperContainer}>
-        <div className={style.imageContainer}>
-          <img
-            src={image}
-            alt=''
-            height='100px'
-            width='100px'
-            className={style.img}
-          />
-          {/* <Avatar size='large' icon={<UserOutlined />} /> */}
+    // <div className={style.Card}>
+    //   <div className={style.upperContainer}>
+    //     <div className={style.imageContainer}>
+    //       <img
+    //         src={image}
+    //         alt=''
+    //         height='100px'
+    //         width='100px'
+    //         className={style.img}
+    //       />
+    //       {/* <Avatar size='large' icon={<UserOutlined />} /> */}
+    //     </div>
+    //     <div className={style.lowerContainer}>
+    //       <h3>{full_name}</h3>
+    //       <h4>{email}</h4>
+    //       <p>{phone}</p>
+    //       <p>{address}</p>
+    //     </div>
+    //   </div>
+    // </div>
+    <>
+      <div
+        style={{
+          display: "flex",
+          // border: "1px solid red",
+          width: "400px",
+          borderRadius: "10px",
+          overflow: "hidden",
+          height: "300px",
+        }}>
+        <div
+          style={{
+            // border: "1px solid rgba(255, 255, 255, 0.3)",
+            borderRight: "none",
+            width: "30%",
+            zIndex: "100",
+            background: "linear-gradient(to left, #ecf0f1, #b4babb)",
+          }}>
+          <div className={style.imageContainer}>
+            <img
+              src='https://static.vecteezy.com/system/resources/thumbnails/010/260/479/small/default-avatar-profile-icon-of-social-media-user-in-clipart-style-vector.jpg'
+              alt=''
+              height='100px'
+              width='100px'
+              className={style.img}
+            />
+          </div>
         </div>
-        <div className={style.lowerContainer}>
-          <h3>{full_name}</h3>
-          <h4>{email}</h4>
-          <p>{phone}</p>
-          <p>{address}</p>
+        <div
+          style={{
+            zIndex: "0",
+            border: "1px solid rgba(183, 183, 183, 0.3)",
+            borderLeft: "none",
+            flex: "1",
+            display: "flex",
+            overflow: "hidden",
+            justifyContent: "center",
+            alignItems: "center",
+            position: "relative",
+          }}>
+          <div
+            style={{
+              // border: "1px solid red",
+              marginLeft: "2.5vw",
+              overflow: "hidden",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "start",
+              height: "100%",
+              position: "absolute",
+            }}>
+            <h3>A Large Heading</h3>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Corrupti, illum!
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
